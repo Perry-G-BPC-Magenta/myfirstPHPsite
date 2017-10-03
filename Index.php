27050fd4517ee8e6a95fd422d1cc40ff5d6accb3
Perry-G-BPC-Magenta <32098875+Perry-G-BPC-Magenta@users.noreply.github.com>
2017-10-03 14:32:54 +0100

    Update Index.php

--- Index.php
+++ Index.php
@@ -10,10 +10,10 @@ Echo "Investigate at least 4 languages for Web server Scripting !";
 //this is me testing this site 
 ?>
   <h3>
-  "1. C++";
-  "2. Python";
-  "3. ASP.NET";
-  "4. Java";
+  1. C++
+  2. Python
+  3. ASP.NET
+  4. Java
   </h3>
 
 </body>
