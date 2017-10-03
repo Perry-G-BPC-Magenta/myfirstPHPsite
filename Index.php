e2e6cb0e29466254c7b18f9e9f8f491d0aecc214
Perry-G-BPC-Magenta <32098875+Perry-G-BPC-Magenta@users.noreply.github.com>
2017-10-03 14:32:10 +0100

    Update Index.php

--- Index.php
+++ Index.php
@@ -4,10 +4,17 @@
 
 <?php
 
-Echo "my first time trying PHP!";
+Echo "Investigate at least 4 languages for Web server Scripting !";
+  
 
 //this is me testing this site 
 ?>
+  <h3>
+  "1. C++";
+  "2. Python";
+  "3. ASP.NET";
+  "4. Java";
+  </h3>
 
 </body>
 </html>
